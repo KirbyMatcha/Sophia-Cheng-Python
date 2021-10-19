33bf6497c3fb1af4e374a149fc6af2c1c811a9d8
KirbyMatcha <sophia.cheng@education.nsw.gov.au>
2021-10-19 15:05:20 +1100

    fix

--- GoodTeacher.py
+++ GoodTeacher.py
@@ -5,4 +5,8 @@ print("|    Name : Sophia                               |")
 print("|    Version : 01                                |")
 print(" ------------------------------------------------")
 username = input("Teacher's name (try Mr Horan) ")
-print(username + " is an ok teacher")
+y = "Mr Horan"
+if username == y:
+    print("You are lucky, he is a great teacher.")
+else:
+    print(username + " is an ok teacher")
