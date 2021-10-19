4855fdf70df05a970552e17cab02736914d8efcb
KirbyMatcha <sophia.cheng@education.nsw.gov.au>
2021-10-19 11:30:26 +1100

    arghh

--- var.py
+++ var.py
@@ -6,3 +6,16 @@ print(x + y)
 a = int(1)
 b = int(2)
 print(a + b)
+
+x = input("Type in a number: ")
+
+y = input("Type in another number: ")
+
+print("The sum of the numbers is " + x + y)
+
+num = int(input("type in a number" ))
+nam =int(input("type in another number" ))
+print(num+nam)
+
+num = input('type in a number')
+numC = int(num)
