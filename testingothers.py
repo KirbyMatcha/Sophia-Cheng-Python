23dd8fbe021e18b6f6ea289cd50fc63f4a2f866e
KirbyMatcha <sophia.cheng@education.nsw.gov.au>
2021-10-20 12:25:25 +1100

    moew

--- testingothers.py
+++ testingothers.py
@@ -1,10 +1,20 @@
-print (" ---------------------------------------------- ")
-print ("|                                              |")
-print ("|    03GoodbyePerson                           |")
-print ("|    Name : Grace Thomas                       |")
-print ("|    Version : 01                              |")
-print ("|                                              |")
-print (" ---------------------------------------------- ")
-print ("Hello")
-value = input("What is your name ? ") 
-print (f'Goodbye {value}')
+a = 25
+b = int(input("Type in a number"))
+c = 22
+if b > a:
+    print("b is greater than a")
+    if b > c:
+        print("b is greater than c")
+    else:
+        print("b is less than c")
+else:
+    print("b is less than a")
+
+if b == 15:
+    print("Wow that's my age")
+
+teacher = input("Teacher's name (try Mr Horan) ")
+if teacher=="Mr Horan":
+    print("You are lucky, he is a great teacher") 
+else:
+    print(teacher,"is an ok teacher")
